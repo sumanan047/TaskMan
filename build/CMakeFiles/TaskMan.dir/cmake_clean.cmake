eb7f939a37d346b9145e2cf1816183007091c4d6
Suman Saurabh <sumanan047@gmail.com>
2025-03-04 23:37:58 -0600

    first commit

---
+++ build/CMakeFiles/TaskMan.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TaskMan.dir/src/main.c.o"
+  "CMakeFiles/TaskMan.dir/src/main.c.o.d"
+  "TaskMan"
+  "TaskMan.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TaskMan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
