be0d5abc5410c84beae697434534e22a3ffecd46
Suman Saurabh <sumanan047@gmail.com>
2025-03-05 00:03:36 -0600

    Add function to add strings to an array

--- src/main.c
+++ src/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+
+void addStringToArray(char* string, char** array, int size) {
+    int index = 0;
+    while (array[index] != NULL) {
+        index++;
+    }
+    if (index < size) {
+        array[index] = strdup(string);
+    } else {
+        printf("Array is full. Cannot add more strings.\n");
+    }
+}
 
 int main() {
-    printf("Hello, World!\n");
+    char* strings[5] = {NULL};
+    addStringToArray("Hello", strings, 5);
+    addStringToArray("World", strings, 5);
+    addStringToArray("GitHub", strings, 5);
+    addStringToArray("Copilot", strings, 5);
+
+    for (int i = 0; i < 5; i++) {
+        if (strings[i] != NULL) {
+            printf("%s\n", strings[i]);
+        }
+    }
+
     return 0;
 }
